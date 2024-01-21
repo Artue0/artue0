56c16eb73ee640b414f3d38001f8b0695e373ddf
Artue0 <128091223+Artue0@users.noreply.github.com>
2024-01-21 08:22:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var track = document.getElementById("image-track");
+    var enableCode = true; // Flag to control code execution
+    var animationDone = false;
+
+    var handleOnDown = function (event) {
+        track.dataset.mouseDownAt = event.clientX;
+    };
+
+    var handleOnUp = function () {
+        track.dataset.mouseDownAt = "0";
+        track.dataset.prevPercentage = track.dataset.percentage;
+    };
+
+    const handleOnMove = function (event) {
+        if (track.dataset.mouseDownAt === "0" || !enableCode) return;
+
+        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - event.clientX;
+        const maxDelta = window.innerWidth;
+
+        const percentage = (mouseDelta / maxDelta) * -100;
+        const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+        const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+        track.dataset.percentage = nextPercentage;
+
+        track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+        const images = track.getElementsByClassName("image");
+        for (var image of images) {
+            image.style.objectPosition = `${100 + nextPercentage}% center`;
+        }
+    };
+
+    window.addEventListener('mousedown', function (event) {
+        handleOnDown(event);
+        handleImageClick(event);
+    });
+
+    window.addEventListener('touchstart', function (event) {
+        handleOnDown(event.touches[0]);
+    });
+
+    window.addEventListener('mouseup', function (event) {
+        handleOnUp(event);
+    });
+
+    window.addEventListener('touchend', function (event) {
+        handleOnUp(event.touches[0]);
+    });
+
+    window.addEventListener('mousemove', function (event) {
+        handleOnMove(event);
+    });
+
+    window.addEventListener('touchmove', function (event) {
+        handleOnMove(event.touches[0]);
+    });
+
+    function handleImageClick(event) {
+      const selectedImage = event.target;
+      const rect = selectedImage.getBoundingClientRect();
+      const imageCopy = selectedImage.cloneNode(true);
+      if (enableCode) {
+        if (imageCopy.classList.contains('image') && !imageCopy.classList.contains('fullscreen') && !animationDone){
+            enableCode = !enableCode;
+            animationDone = !animationDone;
+            document.body.appendChild(imageCopy);
+            imageCopy.classList.add('fullscreen');
+            imageCopy.style.setProperty('--start-x', rect.left + 'px');
+            document.body.style.overflowY = 'auto';
+        }
+    }
+      if (selectedImage.classList.contains('animatedImage')){
+        imageCopy.style.animation = 'Transition 1.8s ease reverse';
+      }
+      console.log(selectedImage);
+    }
+});
